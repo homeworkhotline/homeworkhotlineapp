746ace6a5de663bba3fb09970ebdadb0aa5b325c
Homework Hotline <homeworkhotlinenashville@gmail.com>
2017-08-23 19:25:37 -0500

    schools fix

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -72,6 +72,6 @@ class SchoolsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def school_params
-      params.require(:school).permit(:name, :address, :city, :state, :zip, :county, :phone, :principal, :principalemail, :sonic, :peds, :census, :ptgs, :appalreg, :titlei, :sbdistrict, :ccdistrict)
+      params.require(:school).permit(:name, :address, :city, :state, :zip, :county, :phone, :principal, :principalemail, :sonic, :peds, :census, :ptgs, :appalreg, :titlei, :sbdistrict, :ccdistrict, :schooltype)
     end
 end
